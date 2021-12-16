e8871315cf4d102ca0bbb964b9fcd3ded7c9878c
Ben <510662506@qq.com>
2021-12-16 11:22:52 +0800

    feat: 更新依赖,并且验证是不是Teleport在SSR框架下造成Hydration node mismatch的问题

--- web/pages/index/render.vue
+++ web/pages/index/render.vue
@@ -1,23 +1,34 @@
 
 <template>
   <div>
-    <van-popover v-model:show="showPopover" :actions="actions">
-    </van-popover>
+    <!-- <van-popover v-model:show="showPopover" :actions="actions">
+      <template #reference>
+        浅色风格
+      </template>
+    </van-popover> -->
+    <popup />
+    hahhahaha
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Popover as VanPopover } from 'vant'
+// import { Popover as VanPopover } from 'vant'
+import popup from './popup'
 
 export default defineComponent({
   components: {
-    VanPopover
+    // VanPopover,
+    popup
   },
   data() {
     return {
       showPopover: false,
-      actions: []
+      actions: [
+      { text: '选项一' },
+      { text: '选项二' },
+      { text: '选项三' },
+      ]
     }
   },
   created() {
